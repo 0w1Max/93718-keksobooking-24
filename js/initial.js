c0060c54dea39df3c7253be91881b1b08eeb5b99
Макс <0w1.go.0w1@gmail.com>
2021-10-23 16:54:38 +0300

    Разделил проект на модули

---
+++ js/initial.js
@@ -0,0 +1,51 @@
+// Длина нового массива объектов
+const ARRAY_LENGTH = 10;
+
+// Заголовок объявления
+const TITLE_LIST = [
+  'Новое объявление',
+  'Новое объявление 2',
+  'Новое объявление 3',
+  'Новое объявление 4',
+  'Новое объявление 5',
+  'Новое объявление 6',
+  'Новое объявление 7',
+  'Новое объявление 8',
+  'Новое объявление 9',
+  'Новое объявление 10',
+];
+
+// Описание объявления
+const DESCRIPTION_LIST = [
+  'Новое описание',
+  'Новое описание 2',
+  'Новое описание 3',
+  'Новое описание 4',
+  'Новое описание 5',
+  'Новое описание 6',
+  'Новое описание 7',
+  'Новое описание 8',
+  'Новое описание 9',
+  'Новое описание 10',
+];
+
+// Тип жилья
+const TYPE_LIST = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+
+// Время заезда и выезда
+const CHECK_TIME = ['12:00', '13:00', '14:00'];
+
+// Удобства
+const FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+// Фотографии жилья
+const PHOTOS_LIST = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+// Author avatar
+const AVATAR_LIST = [];
+const AVATAR_NUMBER = 10;
+
+export {ARRAY_LENGTH, TITLE_LIST, DESCRIPTION_LIST, TYPE_LIST, CHECK_TIME,
+  FEATURES_LIST, AVATAR_LIST, AVATAR_NUMBER, PHOTOS_LIST};
---
+++ js/utils.js
@@ -0,0 +1,44 @@
+import {AVATAR_LIST, AVATAR_NUMBER} from './initial.js';
+
+// Получаем случайное число с плавающей точкой из заданного интервала (min, max)
+function getRandomNumber (min, max, fix = 0) {
+  const randomNumber = Math.random() * (max - min) + min;
+
+  if (min < 0 || min >= max) {
+    return 'Указан неверный диапазон';
+  }
+
+  return Number(randomNumber).toFixed(fix);
+}
+
+// Получаем случайное целое положительное число из заданного интервала (min, max)
+const getRandomInteger = (min, max) => Math.floor(getRandomNumber(min, max));
+
+// Получаем массив случайной длины
+function getRandomArray (array) {
+  const newArray = [];
+  const maxLength = array.length;
+  const arrayLength = getRandomInteger(1, maxLength);
+
+  while (newArray.length < arrayLength) {
+    const indexOfEl = getRandomInteger(0, maxLength - 1);
+    const el = array[indexOfEl];
+
+    if (!newArray.includes(el)) {
+      newArray.push(el);
+    }
+  }
+
+  return newArray;
+}
+
+// Генерируем аватарки и добавляем в массив AVATAR_LIST
+for (let index = 0; index < AVATAR_NUMBER; index++) {
+  if (index < 9) {
+    AVATAR_LIST.push(`img/avatars/user0${index + 1}.png`);
+  } else {
+    AVATAR_LIST.push(`img/avatars/user${index + 1}.png`);
+  }
+}
+
+export {getRandomNumber, getRandomInteger, getRandomArray};
