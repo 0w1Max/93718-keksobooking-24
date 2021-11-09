d51bf70962d7ca87c09213bb37dbb466cb5ead90
Макс <0w1.go.0w1@gmail.com>
2021-11-09 14:04:10 +0300

    Добавил зависимость от типа жилья и цены. Добавил проверку цены

--- js/modules/form.js
+++ js/modules/form.js
@@ -1,8 +1,19 @@
 
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE_LENGTH = 1000000;
+
+const MIN_TYPE_PRICE = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
 
 const titleInput = document.querySelector('#title');
+const priceInput = document.querySelector('#price');
+const typeItems = document.querySelectorAll('#type');
 
 // Проверяем длину введённого заголовка
 titleInput.addEventListener('input', () => {
@@ -17,4 +28,30 @@ titleInput.addEventListener('input', () => {
   }
 
   titleInput.reportValidity();
+});
+
+// Просматриваем коллекцию всех типов жилья
+typeItems.forEach((typeItem) => {
+  // При выборе типа меняем placeholder на минимальную цену у соответствующего типа жилья
+  typeItem.addEventListener('change', (evt) => {
+    priceInput.placeholder = MIN_TYPE_PRICE[evt.target.value];
+
+    // Создаём атрибут минимальной цены
+    priceInput.setAttribute('min', priceInput.placeholder);
+  });
+});
+
+// Проверяем введённую цену за ночь
+priceInput.addEventListener('input', () => {
+  const inputNumber = priceInput.value;
+
+  if (inputNumber < +priceInput.placeholder) {
+    priceInput.setCustomValidity('Слишком низкая цена для этого типа жилья!');
+  } else if (inputNumber > MAX_PRICE_LENGTH) {
+    priceInput.setCustomValidity('Слишком высокая цена для этого типа жилья!');
+  } else {
+    priceInput.setCustomValidity('');
+  }
+
+  priceInput.reportValidity();
 });
